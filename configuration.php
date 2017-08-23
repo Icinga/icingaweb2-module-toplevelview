a7bf9bee8ea768a7ba7afe7191f11f221475f1b1
Markus Frosch <markus.frosch@netways.de>
2017-08-23 16:32:51 +0200

    git init

---
+++ configuration.php
@@ -0,0 +1,22 @@
+<?php
+/* Copyright (C) 2017 NETWAYS GmbH <support@netways.de> */
+
+/** @var \Icinga\Application\Modules\Module $this */
+
+// TODO:
+//$this->providePermission('toplevelview/XXX', $this->translate('Whatever'));
+
+/** @var \Icinga\Web\Navigation\NavigationItem $section */
+$section = $this->menuSection('toplevelview');
+$section
+    ->setLabel('Top Level View')
+    ->setUrl('toplevelview')
+    ->setIcon('sitemap')
+    ->setPriority(20);
+
+/* TODO
+$section->add('view_X', array(
+    'url'   => 'toplevelview/show?name=X',
+    'label' => NAME,
+));
+*/
